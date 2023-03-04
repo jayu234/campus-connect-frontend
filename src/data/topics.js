7841e1b9285c39f6eecd13e12e38aa2a10617ac3
Kashyap Bavadiya <bavadiyakashyap@gmail.com>
2023-03-04 19:06:09 +0530

    Topic and Image

--- src/data/topics.js
+++ src/data/topics.js
@@ -1,44 +1,44 @@
 export const topics = [
-    {
-        _id: 1,
-        label: "Web Developement",
+	{
+		_id: 1,
+		label: "Web Developement",
         imgName: "webdev",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        _id: 2,
-        label: "Computer Programming",
-        imgName: "computer-programming",
+	},
+	{
+		_id: 2,
+		label: "Computer Programming",
+		imgName: "computer-programming",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        _id: 3,
-        label: "System design",
-        imgName: "system-design",
+	},
+	{
+		_id: 3,
+		label: "System design",
+		imgName: "system-design",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        _id: 4,
-        label: "App Developement",
-        imgName: "appdev",
+	},
+	{
+		_id: 4,
+		label: "App Developement",
+		imgName: "appdev",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        _id: 5,
-        label: "Java language",
-        imgName: "javalang",
+	},
+	{
+		_id: 5,
+		label: "Java language",
+		imgName: "javalang",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        _id: 6,
-        label: "Python Programming",
-        imgName: "python-programming",
+	},
+	{
+		_id: 6,
+		label: "Python Programming",
+		imgName: "python-programming",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        _id: 7,
-        label: "Competitive Programming",
-        imgName: "competitive-programming",
+	},
+	{
+		_id: 7,
+		label: "Competitive Programming",
+		imgName: "competitive-programming",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
+	},
 ];
